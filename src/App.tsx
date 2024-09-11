74fa6843ee7e65693c3e7734f228e70ede7d2bc3
Joshua Lopez <joshua.lopez1@maine.edu>
2024-09-11 17:52:08 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                UM COS420 with React Hooks and TypeScript Josh
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Josh
             </p>
         </div>
     );
