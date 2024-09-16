788aa308ad600af82145df231a3fefe746de5edc
Brianna Gannett <brianna.gannett@maine.edu>
2024-09-16 14:24:56 -0400

    Added header, header background, image, list, button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-
+import { Button } from "react-bootstrap";
 function App(): React.JSX.Element {
     return (
         <div className="App">
@@ -11,6 +11,23 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Brianna Gannett. Hello World
             </p>
+            <div style={{ background: "blue" }}>
+                <h1>This is a header.</h1>
+            </div>
+            <img src="src\pic.jpg" alt="A picture of old architecture" />
+            <ul>
+                <li> First Item</li>
+                <li> Second Item</li>
+                <li> Third Item</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                {" "}
+                Log Hello World!
+            </Button>
         </div>
     );
 }
