daa21794c6398bab132126aba895242c7dc1cff8
Obed Appiah <obed.appiah@maine.edu>
2024-09-19 13:07:47 -0400

    work so far

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Obed
+                automatically reload. Obed Hello World
             </p>
         </div>
     );
