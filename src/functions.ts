4cae83ea018cfa3191f541b6fd986da63c1c0f81
Noah-Moring <148168395+Noah-Moring@users.noreply.github.com>
2024-09-21 15:01:07 -0400

    solved function changes

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return ((temperature - 32) * 5) / 9;
 }
 
 /**
@@ -12,7 +12,28 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+     let total = 0;
+    if (first < 0) {
+        first = 0;
+        total += first;
+    } else {
+        total += first;
+    }
+    if (second < 0) {
+        second = 0;
+        total += second;
+    } else {
+        total += second;
+    }
+    if (third < 0) {
+        third = 0;
+        total += third;
+    } else {
+        total += third;
+    }
+
+    return total;
+
 }
 
 /**
@@ -20,7 +41,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +49,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return /\?$/.test(message) ? true : false;
 }
 
 /**
@@ -37,5 +58,15 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    const yLetter = ["YES", "yes"];
+    const nLetter = ["NO", "no"];
+
+    if (yLetter[0] === word || yLetter[1] === word) {
+        return true;
+    } else if (nLetter[0] === word || nLetter[1] === word) {
+        return false;
+    } else {
+        return null;
+    }
+
 }
