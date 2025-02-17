39ef483802692ca404eff55bc2ca2c965aea2c00
Alex Picard <alex.m.picard@maine.edu>
2025-02-17 10:14:10 -0500

    Added chapter 2 code. Passes all tests.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,6 +8,31 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <div style={{ backgroundColor: "red" }}>
+                <h1 style={{ backgroundColor: "rgb(255,0,0)" }}>
+                    Heading is added.
+                </h1>
+                ;
+                <img src="/logo192.png" alt="React logo" />;
+                <ul>
+                    <li>Python</li>
+                    <li>C</li>
+                    <li>Java</li>
+                </ul>
+                ;
+                <div>
+                    <Button
+                        variant="primary"
+                        onClick={() => {
+                            console.log("Hello World!");
+                        }}
+                    >
+                        Log Hello World
+                    </Button>
+                </div>
+                ;
+            </div>
+            );
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
