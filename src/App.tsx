c62d9ead03b33a9afd2dc5c6fb30ee02e56ecd47
RexTheMyth <rexthemyth@gmail.com>
2025-05-21 18:27:32 -0400

    ya

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import boat from "./Assets/Boat.jpg";
 
 function App(): React.JSX.Element {
     return (
@@ -7,9 +8,25 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1>This is a Heading</h1>
+            <img src={boat} alt="a picture of a boat" />
+            <ul>
+                <li>Thing 1</li>
+                <li>Thing 2</li>
+                <li>Thing 4</li>
+            </ul>
+            <button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </button>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World Signed: Braden Pare
+                automatically reload.{" "}
+                <span style={{ backgroundColor: "red" }}> Hello World </span>
+                Signed: Braden Pare
             </p>
         </div>
     );
