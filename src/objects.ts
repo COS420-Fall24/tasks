6e43bee3c3bd378f12d80a84819e28ffe465f7d5
RexTheMyth <rexthemyth@gmail.com>
2025-05-22 15:10:23 -0400

    question didnt mention the ID change

--- src/objects.ts
+++ src/objects.ts
@@ -122,7 +122,13 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    let newQuest: Question = {
+        ...oldQuestion,
+        name: "Copy of " + oldQuestion.name,
+        published: false,
+        id: id,
+    };
+    return newQuest;
 }
 
 /**
