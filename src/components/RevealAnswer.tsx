0632a9de6fc688b6f2c6aa8056ca2319b360f712
RexTheMyth <rexthemyth@gmail.com>
2025-05-24 12:52:22 -0400

    life is 42

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visable, setVisiable] = useState<boolean>(false);
+
+    function InvrtVis(): void {
+        setVisiable(!visable);
+    }
+
+    return (
+        <div>
+            <Button onClick={InvrtVis}>Reveal Answer</Button>;
+            {visable && <div>42</div>};
+        </div>
+    );
 }
