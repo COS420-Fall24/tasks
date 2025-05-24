03363759f71702dbe1660978aa358fbb1eedf918
RexTheMyth <rexthemyth@gmail.com>
2025-05-24 11:45:54 -0400

    merge time

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import boat from "./Assets/Boat.jpg";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { TwoDice } from "./components/TwoDice";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): React.JSX.Element {
     return (
@@ -15,7 +12,6 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
             <h1>This is a Heading</h1>
             <img src={boat} alt="a picture of a boat" />
             <ul>
@@ -30,22 +26,15 @@ function App(): React.JSX.Element {
             >
                 Log Hello World
             </button>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.{" "}
-                <span style={{ backgroundColor: "red" }}> Hello World </span>
-                Signed: Braden Pare
-            </p>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
             <TwoDice></TwoDice>
-            <hr />
+            <hr /> <span style={{ backgroundColor: "red" }}> Hello World </span>
+            Signed: Braden Pare
             <ChangeType></ChangeType>
->>>>>>> upstream/task-state
         </div>
     );
 }
