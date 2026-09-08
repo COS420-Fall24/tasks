427876d2f9332f1afaabf5055e5b0e08c278259a
apex04 <119907799+Apex-04@users.noreply.github.com>
2026-09-08 12:00:30 -0400

    added test change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App(): React.JSX.Element {
         <div className="App">
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
-            </header>
+        </header>
+        <h2>Andrew Bobay Fork</h2>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
